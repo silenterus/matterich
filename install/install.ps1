bf95168187a108fd47bd9bbe25e7ca074644b872
Your Name <you@example.com>
2023-10-27 17:29:45 +0200

    more

--- install/install.ps1
+++ install/install.ps1
@@ -98,9 +98,87 @@ try {
 
 
         if($hasCreatedEnv){
+             & pip install -r  requirements.txt
+             & pip install -r  req.txt
+             & pip install -r  requirements-dev.txt
 
 
         }
+    } else {
+        throw "No valid Python executable found."
+    }
+} catch {
+    Write-Host "An error occurred: $_"
+}
+
+Function GetPythonPath {
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        [Parameter(Mandatory=$false)]
+        [string][Alias('v')]$Version,
+
+        [Parameter(Mandatory=$false)]
+        [string][Alias('p')]$PythonPath
+    )
+
+    try {
+        foreach ($Path in @("C:\\Python3\\python.exe", "C:\\Python310\\python.exe", "C:\\Python311\\python.exe")) {
+            if (Test-Path $Path) {
+                return $Path
+            }
+        }
+
+        foreach ($ExecName in @("python", "python310", "python311", "python3")) {
+            $PythonExecPath = (Get-Command -Name $ExecName -ErrorAction SilentlyContinue).Path
+            if ($PythonExecPath -ne $null) {
+                return $PythonExecPath
+            }
+        }
+
+        throw "No valid Python executable found."
+    } catch {
+        Write-Host "An error occurred: $_"
+        return $null
+    }
+}
+
+Function ManageVenv {
+    param (
+        [string]$PythonPath
+    )
+
+    try {
+        if (Test-Path ".\venv") {
+            Remove-Item -Recurse -Force ".\venv"
+            Write-Host "Old virtual environment removed."
+        }
+
+        & $PythonPath -m venv venv
+
+        $activationScripts = @(".\venv\Scripts\Activate", ".\venv\Scripts\activate.ps1", ".\venv\Scripts\activate.bat")
+        $activated = $false
+        foreach ($script in $activationScripts) {
+            if (Test-Path $script) {
+                & $script
+                $activated = $true
+                break
+            }
+        }
+
+        if (-not $activated) {
+            throw "Failed to activate virtual environment."
+        }
+
+        Write-Host "Virtual environment activated successfully."
+    } catch {
+        Write-Host "An error occurred: $_"
+    }
+}
+
+try {
+    $pathPython = GetPythonPath
+    if ($pathPython -ne $null) {
+        ManageVenv -PythonPath $pathPython
     } else {
         throw "No valid Python executable found."
     }
